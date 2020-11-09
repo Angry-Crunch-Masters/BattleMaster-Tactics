4a8abe54231627ca62c32ee3d1073d5b6bdd2f53
Laurentio Verde <laurentioverde@gmail.com>
2020-11-09 22:16:41 +0100

    Method for checking surface boundry is implemented.

--- graphics/canvas.go
+++ graphics/canvas.go
@@ -31,11 +31,20 @@ func (canvas *Canvas) Fill(clr color.Color) {
 
 //DrawImage draws image on canvas
 func (canvas *Canvas) DrawImage(img *ebiten.Image, x, y float64) {
-	options := &ebiten.DrawImageOptions{}
-	options.GeoM.Translate(x, y)
-	canvas.surface.DrawImage(img, options)
+	width, height := img.Size()
+	if canvas.isInBoundary(x, y, float64(width), float64(height)) {
+		options := &ebiten.DrawImageOptions{}
+		options.GeoM.Translate(x, y)
+		canvas.surface.DrawImage(img, options)
+	}
 }
 
 func (canvas *Canvas) isInBoundary(x, y, width, height float64) bool {
-	return true
+	canvasWidth, canvasHeight := canvas.surface.Size()
+	canvasX, canvasY := 0.0, 0.0
+	if canvasX < x+width && canvasX+float64(canvasWidth) > x &&
+		canvasY < y+height && canvasY+float64(canvasHeight) > y {
+		return true
+	}
+	return false
 }
